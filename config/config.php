803e35a09f4ca6615e44994703e30bb7078208a9
srhoades28 <srhoades28@gmail.com>
2015-05-08 11:55:07 -0600

    Changing the configuration

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 //DB Params
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "L5nZ63LG8qY6EsWh");
-define("DB_NAME", "crunch");
+define("DB_PASS", "Your Password Here");
+define("DB_NAME", "Your Database Name");
 
 define("SITE_TITLE", "Welcome to CrunchOff!");
 
